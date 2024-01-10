473fff22e435668a0d0d5e675ebd2d070bee336a
Laura Heino <lauramainen@gmail.com>
2024-01-10 20:31:22 +0200

    Content and styles for front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,32 @@
 import ContainerWithNavigation from "@/components/containerWithNavigation/containerWithNavigation"
 import styles from "./page.module.css"
+import Link from "next/link"
+import Header from "@/components/header/header"
 
 const Home = () => {
-  return <ContainerWithNavigation></ContainerWithNavigation>
+  return (
+    <ContainerWithNavigation>
+      <Header title="Index of /laura_heino/portfolio" alignment="left" />
+      <div>
+        <div className={styles.tableHeading}>
+          <div>Name</div>
+          <div>Description</div>
+        </div>
+        <Link className={styles.link} href="/sweeper">
+          <div className={styles.tableRow}>
+            <p>🚩 Project: Minesweeper game</p>
+            <p>Reverse engineering project</p>
+          </div>
+        </Link>
+        <Link className={styles.link} href="/sweeper">
+          <div className={styles.tableRow}>
+            <p>💌 Contact</p>
+            <p>Email address and such</p>
+          </div>
+        </Link>
+      </div>
+    </ContainerWithNavigation>
+  )
 }
 
 export default Home
