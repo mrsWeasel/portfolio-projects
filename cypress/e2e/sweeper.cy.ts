ae4b43be41cf1493a7ff2cfe8e9e0ea74fcfbb81
Laura Heino <lauramainen@gmail.com>
2024-01-27 11:25:21 +0200

    Assert in e2e tests that timer stops after game over

--- cypress/e2e/sweeper.cy.ts
+++ cypress/e2e/sweeper.cy.ts
@@ -5,6 +5,15 @@ const assertToolBar = () => {
   cy.get('[data-test-id="sweeper-toolbar-timer"]').should("contain", "00:00:00")
 }
 
+const assertTimerIsStopped = () => {
+  cy.get('[data-test-id="sweeper-toolbar-timer"]').then(($timer): void => {
+    const time1 = $timer.text()
+    // Time shouldn't have changed
+    cy.wait(2000)
+    cy.get('[data-test-id="sweeper-toolbar-timer"]').contains(time1.toString())
+  })
+}
+
 const id = "64a0629d6cd31da8c69f4f67"
 const mines = [0, 11, 22, 33, 44, 55, 66, 77, 88, 99]
 
@@ -72,6 +81,9 @@ describe("Sweeper game", () => {
     // Assert leaderboard
     cy.get('[data-test-id="scores-list"]').should("exist")
     cy.get('[data-test-id="game-64a0629d6cd31da8c69f4f67"]').should("exist")
+
+    // Assert timer
+    assertTimerIsStopped()
   })
 
   it("Wins game when all non-mined cells are revealed, last clicked item being empty (no number)", () => {
@@ -115,6 +127,9 @@ describe("Sweeper game", () => {
     // Assert leaderboard
     cy.get('[data-test-id="scores-list"]').should("exist")
     cy.get('[data-test-id="game-64a0629d6cd31da8c69f4f67"]').should("exist")
+
+    // Assert timer
+    assertTimerIsStopped()
   })
 
   it("Loses when hits a cell with mine", () => {
@@ -142,6 +157,9 @@ describe("Sweeper game", () => {
     // Assert leaderboard
     cy.get('[data-test-id="scores-list"]').should("exist")
     cy.get('[data-test-id="game-64a0629d6cd31da8c69f4f67"]').should("not.exist")
+
+    // Assert timer
+    assertTimerIsStopped()
   })
 
   it("Toggles between clicking cells open and marking them with flag", () => {
