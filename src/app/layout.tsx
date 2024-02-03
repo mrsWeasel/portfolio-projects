1e28c7438c6765be18879e1ab79f08d790f2382c
Laura Heino <lauramainen@gmail.com>
2024-02-03 15:15:07 +0200

    Make using fonts more DRY

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import "../styles/globals.css"
-import { Red_Hat_Text } from "next/font/google"
-
-const redHatText = Red_Hat_Text({ subsets: ["latin"], weight: ["400", "700"] })
+import { redHatText } from "@/services/fonts"
 
 export const metadata = {
   title: "Laura Heino | Projects",
